e028c92dd46206bd22f77f4c50be402638f6111d
MarcoMigl <118677046+MarcoMigl@users.noreply.github.com>
2026-01-12 18:00:14 +0100

    Create ControllerCreazioneStrutture.java

---
+++ Codice/controller/ControllerCreazioneStrutture.java
@@ -0,0 +1,182 @@
+package appolloni.migliano.controller;
+
+import appolloni.migliano.entity.Host;
+import appolloni.migliano.entity.Struttura;
+import appolloni.migliano.entity.Utente;
+import appolloni.migliano.factory.FactoryDAO;
+import appolloni.migliano.factory.FactoryStrutture;
+import appolloni.migliano.factory.FactoryUtenti;
+import appolloni.migliano.interfacce.InterfacciaDaoStruttura;
+import appolloni.migliano.interfacce.InterfacciaUtente;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import appolloni.migliano.ClassiDiErrori.CampiVuotiException;
+import appolloni.migliano.ClassiDiErrori.EmailNonValidaException;
+import appolloni.migliano.bean.BeanUtenti;
+import appolloni.migliano.bean.BeanStruttura;
+
+
+
+public class ControllerCreazioneStrutture {
+
+  
+   private InterfacciaDaoStruttura daoStrutture = FactoryDAO.getDAOStrutture();
+   private InterfacciaUtente daoUtente = FactoryDAO.getDaoUtente();
+    public void CreaStruttura(BeanUtenti bean, BeanStruttura beanStr) throws CampiVuotiException,Exception,SQLException{
+
+      String type = beanStr.getTipo();
+      String nomeStruttura = beanStr.getName();
+      String citta = beanStr.getCitta();
+      String indirizzo = beanStr.getIndirizzo();
+      String orario = beanStr.getOrario();
+      boolean wifi = beanStr.hasWifi();
+      boolean ristorazione = beanStr.hasRistorazione();
+      String responsabile = beanStr.getGestore();
+      String tipoAtt = beanStr.getTipoAttivita();
+      String foto = beanStr.getFoto();
+      
+      try{
+        
+        if(type.isEmpty() || nomeStruttura.isEmpty() || citta.isEmpty()|| indirizzo.isEmpty() || orario.isEmpty() ||responsabile.isEmpty()|| tipoAtt.isEmpty()){
+          throw new CampiVuotiException("Completa tutti i campi.");
+        }
+
+       Struttura nuovStruttura =  daoStrutture.cercaStruttura(nomeStruttura,responsabile);
+       if( nuovStruttura != null){
+         throw new Exception("La struttura esiste");
+       }else{
+
+        Struttura struttura = FactoryStrutture.creazioneStrutture(type, nomeStruttura, citta, indirizzo, orario,wifi, ristorazione, tipoAtt, responsabile);
+        
+       if (struttura == null) {
+             throw new Exception("Creazione struttura fallita.");
+       }
+
+       struttura.setFoto(foto);
+
+        String emailHost = null;
+
+      if(bean.getTipo().equals("Host")){
+        
+        emailHost = bean.getEmail();
+      }else{
+        emailHost = null;
+      }
+      daoStrutture.salvaStruttura(struttura,emailHost);
+
+      }
+    }catch(IOException e){
+      throw e;
+    
+    }catch(SQLException e){
+
+      e.printStackTrace();
+      throw e;
+    }
+
+  
+
+  }
+
+   public void creazioneUtente(BeanUtenti bean) throws Exception,SQLException, CampiVuotiException, EmailNonValidaException{
+        String type = bean.getTipo(); 
+        String nome = bean.getName();
+        String cognome = bean.getCognome(); 
+        String email = bean.getEmail(); 
+        String citta = bean.getCitta(); 
+        String password = bean.getPassword();
+
+      if(type.isEmpty() || nome.isEmpty() || cognome.isEmpty() || email.isEmpty()|| citta.isEmpty()|| password.isEmpty()){
+        throw new CampiVuotiException("Dati mancanti, inserire i dati in tutti i campi");
+
+      }
+
+      if(!email.contains("@")){
+
+        throw new EmailNonValidaException("Inserire una email valida");
+      }
+
+      try{
+        Utente esistente = daoUtente.cercaUtente(email);
+        
+        if(esistente != null){
+            throw new Exception("Utente già esistente");
+        }
+
+        Utente utente = FactoryUtenti.Creazione(type, nome, cognome, email, citta,password);
+
+        
+        if(utente == null){
+            throw new Exception("Creazione utente fallita");
+        }
+
+
+        if(bean.getTipo().equals("Host")){
+            ((Host)utente).setNomeAttivita(bean.getNomeAttivita());
+            ((Host)utente).setTipoAttivita(bean.getTipoAttivita());
+        }
+
+        daoUtente.salvaUtente(utente);
+      }catch(SQLException e){
+        throw e;
+
+      }
+
+    }
+
+
+    public BeanStruttura visualizzaStrutturaHost(String emailHost) throws Exception {
+        if(emailHost.isEmpty() || emailHost == null ){
+            throw new Exception("Host non vaido");
+
+        }
+        BeanStruttura beanStruttura = null;
+        try{
+         Struttura struttura = daoStrutture.recuperaStrutturaPerHost(emailHost);
+
+         beanStruttura = new BeanStruttura(struttura.getTipo(), struttura.getName(), struttura.getCitta(), struttura.getIndirizzo(),struttura.getOrario(), struttura.hasWifi(), struttura.hasRistorazione(), struttura.getTipoAttivita(), struttura.getGestore());
+         beanStruttura.setFoto(struttura.getFoto());
+        }catch(SQLException e){
+            e.printStackTrace();
+            throw e;
+
+        }catch(IOException e){
+            throw e;
+
+        }
+        return beanStruttura;
+
+    }
+
+    public void cambiaFoto(String emailHost, String nomeFoto) throws CampiVuotiException, SQLException, Exception, IOException {
+     if (emailHost == null || nomeFoto == null) throw new CampiVuotiException("Dati mancanti");
+     try {
+      daoStrutture.aggiornaFotoStruttura(emailHost, nomeFoto);
+     }catch(SQLException e){
+        e.printStackTrace();
+        throw e;
+     }catch(IOException e){
+        throw e;
+     }catch(Exception e){
+        throw e;
+     }
+    }
+
+    public void aggiornaStruttura(BeanStruttura struttura, String vecchionNome) throws Exception, SQLException, CampiVuotiException{
+        if(struttura.getCitta().isEmpty() || struttura.getGestore().isEmpty() || struttura.getIndirizzo().isEmpty()|| struttura.getName().isEmpty()|| struttura.getOrario().isEmpty()||struttura.getTipoAttivita().isEmpty()){
+            throw new CampiVuotiException("Dati mancanti");
+        }
+        Struttura struttura2 = new Struttura(struttura.getTipo(), struttura.getName(), struttura.getCitta(), struttura.getIndirizzo(), struttura.getOrario(), struttura.hasWifi(), struttura.hasRistorazione(), struttura.getTipoAttivita(), struttura.getGestore());
+        try{
+         daoStrutture.updateStruttura(struttura2, vecchionNome);
+        }catch(SQLException e){
+            e.printStackTrace();
+            throw e;
+        }catch(IOException e){
+            throw e;
+        }
+    }
+    
+
+}
