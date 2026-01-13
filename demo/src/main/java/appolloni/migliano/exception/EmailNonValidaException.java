c091855263e3c6116103ca788f5297e298e8a76b
MarcoMigl <118677046+MarcoMigl@users.noreply.github.com>
2026-01-13 12:04:35 +0100

    Create EmailNonValidaException.java

---
+++ demo/src/main/java/appolloni/migliano/exception/EmailNonValidaException.java
@@ -0,0 +1,9 @@
+package appolloni.migliano.exception;
+
+public class EmailNonValidaException extends Exception {
+
+    public EmailNonValidaException(String mess){
+        super(mess);
+    }
+    
+}
