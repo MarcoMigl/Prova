9e287ad1219fe8a2fd9a87c2e10bf5808232b0f8
MarcoMigl <118677046+MarcoMigl@users.noreply.github.com>
2026-01-13 12:04:05 +0100

    Create CredenzialiSbagliateException.java

---
+++ demo/src/main/java/appolloni/migliano/exception/CredenzialiSbagliateException.java
@@ -0,0 +1,7 @@
+package appolloni.migliano.exception;
+
+public class CredenzialiSbagliateException extends Exception{
+    public CredenzialiSbagliateException(String mess){
+        super(mess);
+    }
+}
