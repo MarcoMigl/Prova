8e32a57fe91afe0b10ab51f9a146ac85a2f756d2
MarcoMigl <118677046+MarcoMigl@users.noreply.github.com>
2026-01-13 13:21:48 +0100

    Update Launcher.java

--- demo/src/main/java/appolloni/migliano/Launcher.java
+++ demo/src/main/java/appolloni/migliano/Launcher.java
@@ -3,6 +3,7 @@ package appolloni.migliano;
 public class Launcher {
     public static void main(String[] args) {
         // Chiama il main della tua vera applicazione
-        Main.main(args);
+        MainCLI.main(args);
     }
+
 }
