41fd6d3b1fe4f1ae939d74b292e9f2fcbbe18bed
MarcoMigl <118677046+MarcoMigl@users.noreply.github.com>
2026-01-13 13:20:32 +0100

    Update FactoryDAO.java

--- demo/src/main/java/appolloni/migliano/factory/FactoryDAO.java
+++ demo/src/main/java/appolloni/migliano/factory/FactoryDAO.java
@@ -1,88 +1,106 @@
 package appolloni.migliano.factory;
 
-import java.sql.Connection;
 import java.sql.SQLException;
 
 import appolloni.migliano.Configurazione;
+import appolloni.migliano.DAO.DaoGruppo.DaoGruppoDB;
+import appolloni.migliano.DAO.DaoGruppo.DaoGruppoDemo;
+import appolloni.migliano.DAO.DaoMessaggi.DaoMessaggioDB;
+import appolloni.migliano.DAO.DaoMessaggi.DaoMessaggioDemo;
+import appolloni.migliano.DAO.DaoRecensioni.DaoRecensioniDB;
+import appolloni.migliano.DAO.DaoRecensioni.DaoRecensioniDemo;
+import appolloni.migliano.DAO.DaoRecensioni.DaoRecensioniFile;
+import appolloni.migliano.DAO.DaoStrutture.DAOStruttureDB;
+import appolloni.migliano.DAO.DaoStrutture.DAOStruttureDemo;
+import appolloni.migliano.DAO.DaoStrutture.DAOStruttureFILE;
+import appolloni.migliano.DAO.DaoUtente.DaoUtenteDB;
+import appolloni.migliano.DAO.DaoUtente.DaoUtenteDEMO;
 import appolloni.migliano.DBConnection;
-import appolloni.migliano.dao.gruppo.*;
-import appolloni.migliano.dao.messaggio.DaoMessaggioDB;
-import appolloni.migliano.dao.messaggio.DaoMessaggioDemo;
-import appolloni.migliano.dao.recensione.DaoRecensioniDB;
-import appolloni.migliano.dao.recensione.DaoRecensioniDemo;
-import appolloni.migliano.dao.recensione.DaoRecensioniFile;
-import appolloni.migliano.dao.strutture.DAOStruttureDB;
-import appolloni.migliano.dao.strutture.DAOStruttureDemo;
-import appolloni.migliano.dao.strutture.DAOStruttureFILE;
-import appolloni.migliano.dao.utente.DaoUtenteDB;
-import appolloni.migliano.dao.utente.DaoUtenteDEMO;
 import appolloni.migliano.interfacce.InterfacciaDaoRecensioni;
 import appolloni.migliano.interfacce.InterfacciaDaoStruttura;
 import appolloni.migliano.interfacce.InterfacciaGruppo;
 import appolloni.migliano.interfacce.InterfacciaMessaggi;
 import appolloni.migliano.interfacce.InterfacciaUtente;
 
-public class FactoryDAO {
-    
-    
-    private static final String TIPO = Configurazione.getTipoPersistenza();
-    private static final String ERRMSG = "Impossibile connettersi al Database";
 
-    private FactoryDAO() {
-        throw new IllegalStateException("Utility class");
-    }
+public class FactoryDAO {
+    private static final String tipo = Configurazione.getTipoPersistenza();
 
     public static InterfacciaDaoStruttura getDAOStrutture() {
-        if (Configurazione.JDBC.equals(TIPO)) {
-            return new DAOStruttureDB(getConn());
-        } else if (Configurazione.DEMO.equals(TIPO)) {
-            return new DAOStruttureDemo();
-        } else {
-            return new DAOStruttureFILE();
+    try {
+        if(tipo.equals(Configurazione.JDBC)){
+            return new DAOStruttureDB(DBConnection.getConnection());
         }
+    } catch (SQLException e) {
+        e.printStackTrace();
+        // Qui potresti decidere di lanciare una RuntimeException per non 
+        // dover aggiungere 'throws' ovunque, ma segnalare comunque l'errore
+        throw new RuntimeException("Errore critico di connessione al DB", e);
     }
 
+    if(tipo.equals(Configurazione.DEMO)){
+        return new DAOStruttureDemo();
+    } else {
+        return new DAOStruttureFILE();
+    }
+}
+
     public static InterfacciaDaoRecensioni getDaoRecensioni() {
-        if (Configurazione.JDBC.equals(TIPO)) {
-            return new DaoRecensioniDB(getConn());
-        } else if (Configurazione.DEMO.equals(TIPO)) {
+        try {
+            if (tipo.equals(Configurazione.JDBC)) {
+            return new DaoRecensioniDB(DBConnection.getConnection());
+            }
+        } catch (SQLException e) {
+       e.printStackTrace();
+       throw new RuntimeException("Errore critico di connessione al DB", e);
+    }
+
+    if(tipo.equals(Configurazione.DEMO)){
             return new DaoRecensioniDemo();
         } else {
             return new DaoRecensioniFile();
         }
-    }
+}
     
-    public static InterfacciaGruppo getDaoGruppo() {
-        if (Configurazione.JDBC.equals(TIPO)) {
-            return new DaoGruppoDB(getConn());
-        } else {
-            return new DaoGruppoDemo();
-        }
-    }
-
-    public static InterfacciaMessaggi getDaoMessaggi() {
-        if (Configurazione.JDBC.equals(TIPO)) {
-            return new DaoMessaggioDB(getConn());
-        } else {
-            return new DaoMessaggioDemo();
-        }
-    }
-
-    public static InterfacciaUtente getDaoUtente() {
-        if (Configurazione.JDBC.equals(TIPO)) {
-            return new DaoUtenteDB(getConn());
-        } else {
-            return new DaoUtenteDEMO();
-        }
-    }
-
-
-    private static Connection getConn() {
+    public static InterfacciaGruppo getDaoGruppo(){
         try {
-            return DBConnection.getConnection();
+            if(tipo.equals(Configurazione.JDBC)){
+            return new DaoGruppoDB(DBConnection.getConnection());
+            }
         } catch (SQLException e) {
-          
-            throw new IllegalAccessError(ERRMSG);
+            e.printStackTrace();
+            throw new RuntimeException("Errore di connesione al DB", e);
         }
+                return new DaoGruppoDemo();
+            
+        
+    }
+
+    public static InterfacciaMessaggi getDaoMessaggi(){
+        try {
+            if(tipo.equals(Configurazione.JDBC)){
+            return new DaoMessaggioDB(DBConnection.getConnection());
+
+        }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Errore di connessione al DB", e);
+        }
+            return new DaoMessaggioDemo();
+
+        }
+    
+
+    public static InterfacciaUtente getDaoUtente(){
+        try {
+            if(tipo.equals(Configurazione.JDBC)){
+            return new DaoUtenteDB(DBConnection.getConnection());
+        }
+            
+        } catch (SQLException e) {
+        }
+            return new DaoUtenteDEMO();
+        
     }
 }
+
