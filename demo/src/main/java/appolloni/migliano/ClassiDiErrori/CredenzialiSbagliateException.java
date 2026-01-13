f2c5885999b69fb0c73bc3c2b293c33103a75c7f
MarcoMigl <118677046+MarcoMigl@users.noreply.github.com>
2026-01-13 13:31:28 +0100

    Update CredenzialiSbagliateException.java

--- demo/src/main/java/appolloni/migliano/ClassiDiErrori/CredenzialiSbagliateException.java
+++ demo/src/main/java/appolloni/migliano/ClassiDiErrori/CredenzialiSbagliateException.java
@@ -1,4 +1,4 @@
-package appolloni.migliano.exception;
+package appolloni.migliano.ClassiDiErrori;
 
 public class CredenzialiSbagliateException extends Exception{
     public CredenzialiSbagliateException(String mess){
