d0cb7b5224c85abec6267ebde0022119fbd214b8
MarcoMigl <118677046+MarcoMigl@users.noreply.github.com>
2026-01-17 16:49:33 +0100

    Delete demo/src/main/java/appolloni/migliano/Launcher.java

--- demo/src/main/java/appolloni/migliano/Launcher.java
+++
@@ -1,9 +0,0 @@
-package appolloni.migliano;
-
-public class Launcher {
-    public static void main(String[] args) {
-        // Chiama il main della tua vera applicazione
-        MainCLI.main(args);
-    }
-
-}
